5941572a899a14313f42bddbc4862f459d0c33a8
Vitaly <novaua@users.noreply.github.com>
2026-05-15 11:50:42 +0200

    LiChessConnector Added

---
+++ ChessQtTests/ChessQtTests.cpp
@@ -0,0 +1,49 @@
+#include <QtTest>
+#include "LichessClient.h"
+
+class ChessQtTests : public QObject
+{
+    Q_OBJECT
+
+private slots:
+
+    void initTestCase_data()
+    {
+        qDebug("Creates a global test data table.");
+    }
+
+    void initTestCase()
+    {
+        qDebug("Called before the first test function is executed.");
+    }
+
+    void init()
+    {
+        qDebug("Called before each test function is executed.");
+    }
+
+    void myTest()
+    {
+        QVERIFY(true); // check that a condition is satisfied
+        QCOMPARE(1, 1); // compare two values
+    }
+    
+	void meBasicQtTest()
+	{
+        auto id = LichessClient::gameIdFromUrl("https://lichess.org/AbCdEfGh");
+        QCOMPARE(QString("AbCdEfGh"), id);
+	}
+
+    void cleanup()
+    {
+        qDebug("Called after every test function.");
+    }
+
+    void cleanupTestCase()
+    {
+        qDebug("Called after the last test function was executed.");
+    }
+};
+
+QTEST_MAIN(ChessQtTests)
+#include "ChessQtTests.moc"
